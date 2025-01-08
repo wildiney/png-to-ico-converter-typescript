5d3c419bee4756c6ecac1823aa8f73d594f457c1
Wildiney Di Masi <wildiney@gmail.com>
2025-01-08 10:57:20 -0300

    update packages

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,9 @@
-import { prompt } from 'inquirer'
+import input from '@inquirer/input';
 import { getPNGfiles, convert } from './convert-png-to-ico'
 
-const questions = [
-  {
-    type: 'input',
-    name: 'directory',
-    message: 'Enter the directory to convert'
-  }
-]
-
 export async function main () {
-  const answers = await prompt(questions)
-  const { directory } = answers
+  const answers = await input({ 'message': 'Enter the directory to convert' })
+  const directory = answers
   const filesInDir = getPNGfiles(directory)
   if (filesInDir?.length === 0) {
     throw new Error('No file ending with .png found in the directory')
